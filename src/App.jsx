3e897fab77200e58f386176ae7289519503d8a10
RodrigoHonorio44 <rodrigohono43@outlook.com>
2026-06-22 21:26:26 -0300

    Fix: Ajustando case-sensitivity do import do Firebase para o build do Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, useState, useMemo } from "react";
 // Corrigido para minúsculo para evitar erro de case-sensitivity
-import { auth, db } from "./api/firebase"; 
+import { auth, db } from "./api/Firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { ToastContainer, toast } from "react-toastify";
